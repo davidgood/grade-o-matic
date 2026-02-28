7dc666b0fc5eb688210ece635d47b5cef9b34076
David Good <dgood@programminggoody.com>
2026-02-27 23:31:05 -0500

    fix data type of id

--- src/domains/assignments/dto/assignment_dto.rs
+++ src/domains/assignments/dto/assignment_dto.rs
@@ -2,7 +2,7 @@ use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct AssignmentSummaryDto {
-    pub id: String,
+    pub id: uuid::Uuid,
     pub title: String,
     pub due_at: Option<String>,
 }
